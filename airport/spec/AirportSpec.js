b2f52793bd1d2307e026858ac92f56547bb4fefa
Paula <pmuldoon@umich.edu>
2017-05-09 16:52:06 +0100

    created empty hangar, tests green

--- airport/spec/AirportSpec.js
+++ airport/spec/AirportSpec.js
@@ -2,8 +2,10 @@
 
 describe('Airport', function() {
   var airport;
+  var plane;
   beforeEach(function() {
     airport = new Airport();
+    plane = jasmine.createSpy('plane', ['land']);
   });
   it('has no planes by default', function() {
     expect(airport.planes()).toEqual([]);
